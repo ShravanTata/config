513e7765f1e9ee4d3109723023a6a81519c3186b
Shravan Tata Ramalingasetty <tatarama@>
2020-12-17 22:03:00 +0100

    [RESTIC] Fixed shell script for backups
    --verbose is not supported for v0.8

--- restic_synology_backup.sh
+++ restic_synology_backup.sh
@@ -1,4 +1,8 @@
-#!/bin/zsh
+#!/bin/bash
 
 export RESTIC_REPOSITORY="sftp:tatarama@172.22.22.92:/backups/linux_pc"
 export RESTIC_PASSWORD="LinuxBackup"
+
+restic backup --files-from /home/tatarama/.config/restic/includes --exclude-file=/home/tatarama/.config/restic/excludes
+
+restic forget --prune --keep-hourly 6 --keep-daily 7 --keep-weekly 4 --keep-monthly 12
