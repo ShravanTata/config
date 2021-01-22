55e3094ed6713aaf7fa1e002d7f8895ce4c10b31
Shravan Tata Ramalingasetty <tatarama@>
2021-01-22 02:31:39 +0100

    [RESTIC] Updated excludes and backup echo

--- restic_synology_backup.sh
+++ restic_synology_backup.sh
@@ -3,10 +3,10 @@
 export RESTIC_REPOSITORY="sftp:tatarama@172.22.22.92:/backups/linux_pc"
 export RESTIC_PASSWORD="LinuxBackup"
 
-echo "Initiating backup... "
+echo $(date) "************************* Initiating backup ************************* "
 
 restic backup --files-from /home/tatarama/.config/restic/includes --exclude-file=/home/tatarama/.config/restic/excludes
 
 restic forget --prune --keep-hourly 6 --keep-daily 7 --keep-weekly 4 --keep-monthly 12
 
-echo "Backup complete... "
+echo $(date) "************************* Backup complete ************************* "
