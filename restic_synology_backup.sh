26baea3c716afe7447b75c426a371c9df1c4b210
Shravan Tata Ramalingasetty <tatarama@>
2021-01-25 16:15:06 +0100

    [RESTIC] Fixed backup script

--- restic_synology_backup.sh
+++ restic_synology_backup.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-export RESTIC_REPOSITORY="sftp:tatarama@172.22.22.92:/backups/linux_pc"
+export RESTIC_REPOSITORY="sftp:restic-server:/backups/linux_pc"
 export RESTIC_PASSWORD="LinuxBackup"
 
-echo $(date) "************************* Initiating backup ************************* "
+echo $(date) "************************* Initiating backup *************************"
 
 restic backup --files-from /home/tatarama/.config/restic/includes --exclude-file=/home/tatarama/.config/restic/excludes
 
 restic forget --prune --keep-hourly 6 --keep-daily 7 --keep-weekly 4 --keep-monthly 12
 
-echo $(date) "************************* Backup complete ************************* "
+echo $(date) "************************* Backup complete *************************"
