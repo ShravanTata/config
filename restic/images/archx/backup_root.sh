bbcfda74b60f20db6a2e33349eed12c9f3d19533
ShravanTata <shravan.tataramalingasetty@epfl.ch>
2022-01-03 00:44:02 +0100

    [RESTIC] Added files for arch backup

---
+++ restic/images/archx/backup_root.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export RESTIC_REPOSITORY="sftp:synology-nas:/home/backups/archx/root"
+export RESTIC_PASSWORD="LinuxBackup"
+
+echo $(date) "************************* Initiating backup *************************"
+
+restic /home/restic/bin/restic backup /boot --exclude-file=/home/restic/config/restic/images/archx/excludes_root -vvv
+
+# restic backup --files-from /home/tatarama/.config/restic/includes --exclude-file=/home/tatarama/.config/restic/excludes
+
+# restic forget --prune --keep-hourly 6 --keep-daily 7 --keep-weekly 4 --keep-monthly 12
+
+
+echo $(date) "************************* Backup complete *************************"
