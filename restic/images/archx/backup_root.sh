d5d78d7ff76034d0207d35d14548d331d93d7da5
ShravanTata <shravan.tataramalingasetty@epfl.ch>
2022-01-08 03:07:11 +0100

    [RESTIC] Changed pop-up messages in archx/backup root

--- restic/images/archx/backup_root.sh
+++ restic/images/archx/backup_root.sh
@@ -5,7 +5,7 @@ echo "# Initiating Backup #"
 echo $(date)
 echo "#####################"
 
-sudo -u tatarama /home/tatarama/.local/bin/restic-notify.sh "Performing restic backup for root" --icon=dialog-information
+sudo -u tatarama /home/tatarama/.local/bin/restic-notify.sh "RESTIC" "Performing backup for root" --icon=dialog-information
 
 # export env variables
 export RESTIC_REPOSITORY="sftp:synology-nas:/home/backups/archx/root"
@@ -21,7 +21,7 @@ then
     echo " "
 else
     echo "[ERROR] Restic respository unavailable, exiting backup ... ";
-    sudo -u tatarama /home/tatarama/.local/bin/restic-notify.sh "Restic root backup failed. Check logs" --icon=dialog-error
+    sudo -u tatarama /home/tatarama/.local/bin/restic-notify.sh "RESTIC" "root backup failed. Check logs" --icon=dialog-error
     exit
 fi
 
@@ -34,7 +34,7 @@ then
     echo " "
 else
     echo "[ERROR] restic-bin does not have the permissions, exiting backup ... ";
-    sudo -u tatarama /home/tatarama/.local/bin/restic-notify.sh "Restic root backup failed. Check logs" --icon=dialog-error
+    sudo -u tatarama /home/tatarama/.local/bin/restic-notify.sh "RESTIC" "root backup failed. Check logs" --icon=dialog-error
     exit
 fi
 
@@ -42,7 +42,7 @@ fi
 
 # /home/restic/bin/restic forget --prune --keep-hourly 6 --keep-daily 7 --keep-weekly 4 --keep-monthly 12
 
-sudo -u tatarama /home/tatarama/.local/bin/restic-notify.sh "Restic root backup finished." --icon=dialog-information
+sudo -u tatarama /home/tatarama/.local/bin/restic-notify.sh "RESTIC" "root backup finished." --icon=dialog-information
 
 echo "##################"
 echo "# Exiting Backup #"
