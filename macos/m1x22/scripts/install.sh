6f93a2f4dd376d36d8a17c3d3b39f306564388a3
Shravan Tata Ramalingasetty <tatarama@m1x22.local>
2022-06-20 22:41:50 -0400

    [M1x22] Added install script

---
+++ macos/m1x22/scripts/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# This script install all the symlinks and necessary packages to setup
+# a reproducible environment that I have been using on the M1 2022 machine
+
+echo "#####################"
+echo "# Initiating setup  #"
+echo $(date)
+echo "#####################"
+
+##################################################
+# stow packages
+##################################################
+stow_packages = (
+
+)
