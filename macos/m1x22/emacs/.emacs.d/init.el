287625fa4893088a3e667516323d6dfd21c7eed4
Shravan Tata <shravantr@gmail.com>
2024-09-23 09:25:05 -0400

    [M1X22] Added brew gcc path for native compilation check

--- macos/m1x22/emacs/.emacs.d/init.el
+++ macos/m1x22/emacs/.emacs.d/init.el
@@ -1,2 +1,27 @@
+
+
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (exec-path-from-shell-initialize)
+  (if (and (fboundp 'native-comp-available-p)
+           (native-comp-available-p))
+      (progn
+        (message "Native comp is available")
+        ;; Using Emacs.app/Contents/MacOS/bin since it was compiled with
+        ;; ./configure --prefix="$PWD/nextstep/Emacs.app/Contents/MacOS"
+        (add-to-list 'exec-path (concat invocation-directory "bin") t)
+        (setenv "LIBRARY_PATH" (concat (getenv "LIBRARY_PATH")
+                                       (when (getenv "LIBRARY_PATH")
+                                         ":")
+                                       ;; This is where Homebrew puts gcc libraries.
+                                       (car (file-expand-wildcards
+                                             (expand-file-name "/opt/homebrew/opt/gcc/lib/gcc/*")))))
+        ;; Only set after LIBRARY_PATH can find gcc libraries.
+        (setq comp-deferred-compilation t))
+    (message "Native comp is *not* available")))
+
+
+
 (require 'org)
 (org-babel-load-file (expand-file-name "~/projects/personal/org/core/init.org"))
